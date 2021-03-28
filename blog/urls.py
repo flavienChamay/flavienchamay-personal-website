bbc979f2dcd1e04295f7905ed0334f55f67180e3
flavienChamay <flavien.chamay@protonmail.com>
2021-03-28 09:59:08 +0200

    New post template created

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,13 @@ This module manages the URLS of the blog application.
 """
 
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 
 urlpatterns = [
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # Linking a blog post with the BlogDetailView view.
-    path('', BlogListView.as_view(), name='home'), # Linking the home page with the BlogListView view.
+    # Linking the creation of a blog post with the BlogCreateView view.
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    # Linking a blog post with the BlogDetailView view.
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    # Linking the home page with the BlogListView view.
+    path('', BlogListView.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ This module manages the views of the blog application.
 from django.views.generic import ListView, DetailView
 from .models import Post
 
+
 class BlogListView(ListView):
     """
     Class view that links the Post class to the home.html file.
@@ -15,18 +16,32 @@ class BlogListView(ListView):
     :var model Post: The model of the ListView.
     :var template_name str: The template used for this view.
     """
-    
+
     model = Post
     template_name = 'home.html'
 
-    
+
 class BlogDetailView(DetailView):
     """
     Class view that links the Post class to the post_detail.html template file.
 
-    :var mode Post: The model of the DetailView.
+    :var model Post: The model of the DetailView.
     :var template_name str: The template used for this view.
     """
-    
+
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    """
+    Class view that links the Post class to the post_new.html template file.
+
+    :var model Post: The model of the CreateView.
+    :var template_name str: The template used for this view.
+    :var fields list: List of the fields used to create a new post.
+    """
+
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'body']
