c867e1ef3b3f3cebc127f5cd1039383ac1776b1e
flavienChamay <flavien.chamay@protonmail.com>
2021-03-30 21:28:17 +0200

    Migrations errors resolved

--- accounts/admin.py
+++ accounts/admin.py
@@ -16,11 +16,23 @@ class CustomUserAdmin(UserAdmin):
     :var add_form CustomUserCreationForm: What form to use when creating an admin.
     :var form CustomUserChangeForm: What form to use when changing the admin.
     :var model CustomUser: The model of the admin.
+    :var list_display list: List of the fields that we can control and see in the administration.
+    :var fieldsets dict: Used to to edit and add new custom fields.
+    :var add_fieldsets dict: Used to to edit and add new custom fields.
     """
+    
     
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['email', 'username', 'age', 'is_staff', ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age', )}),
+    )
+    
 
 # Register the new functionalities into the site.
 admin.site.register(CustomUser, CustomUserAdmin)
