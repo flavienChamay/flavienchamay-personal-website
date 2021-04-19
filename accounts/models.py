8c66e4a5bd230938877cffc0bed3e86a1c5d16fc
flavienChamay <flavien.chamay@protonmail.com>
2021-04-19 20:13:08 +0200

    polishing

--- accounts/models.py
+++ accounts/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
+    """
+    Class that manages the custom user model for the entire site. Even if this site will not use a custom user model we must implement it for further improvements (if needed).
+    """
+    
+    pass
--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from environs import Env # Importing environs package for environment variables
+from environs import Env  # Importing environs package for environment variables
 
 # Environment Variables
 env = Env()
