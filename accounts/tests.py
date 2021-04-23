df1d7d019afd102b6e9d9a5a4e360f09fbec24d5
flavienChamay <flavien.chamay@protonmail.com>
2021-04-23 19:17:38 +0200

    homepage tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+"""
+Module of the tests of the account app.
 
-# Create your tests here.
+:class CustomUserTests: Class implementing tests for the custom user model.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class CustomUserTests(TestCase):
+    """
+    This class inherits from TestCase for testing database of the Post model.
+
+    :method: test_create_user
+    :method: test_create_superuser
+    """
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='will',
+            email='will@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_superuser)
